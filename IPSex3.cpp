b7d8bed46dbc1d21029680a6d329239b917674cc
VladislavShmatovskyi <vlshiv159@gmail.com>
2017-10-21 17:24:40 +0300

    Parallel code add

--- IPSex3.cpp
+++ IPSex3.cpp
@@ -42,11 +42,51 @@ void SerialGaussMethod(double **matrix, const int rows, double* result)
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
 	for (k = 0; k < rows; ++k)
 	{
+
+		/// Функция ParallelGaussMethod() решает СЛАУ методом Гаусса 
+void ParallelGaussMethod(double **matrix, const int rows, double* result)
+{
+	int k;
+	double koef;
+	duration<double> duration; // Перемнная для измерения времени
+					
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	// прямой ход метода Гаусса
+	for (k = 0; k < rows; ++k)
+	{
+		//использование cilk_for во внутреннем цикле прямого хода
+		for (int i = k + 1; i < rows; ++i)
+		{
+			koef = -matrix[i][k] / matrix[k][k];
+			cilk_for(int j = k; j <= rows; ++j)
+			{
+				matrix[i][j] += koef * matrix[k][j];
+			}
+		}
+	}
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration = (t2 - t1);
+	printf("Duration is: %lf seconds\n", duration.count()); // Выводим время работы прямого хода
+
+	// обратный ход метода Гаусса
+	result[rows - 1] = matrix[rows - 1][rows] / matrix[rows - 1][rows - 1];
+
+	for (k = rows - 2; k >= 0; --k)
+	{
+		result[k] = matrix[k][rows];
 		//
+		cilk_for(int j = k + 1; j < rows; ++j)
+			result[k] -= matrix[k][j] * result[j];
+
+
+		result[k] /= matrix[k][k];
+	}
+}//
 		for (int i = k + 1; i < rows; ++i)
 		{
 			koef = -matrix[i][k] / matrix[k][k];
 
+			
 			for (int j = k; j <= rows; ++j)
 			{
 				matrix[i][j] += koef * matrix[k][j];
@@ -82,20 +122,20 @@ int main()
 	int i;
 
 	// кол-во строк в матрице, приводимой в качестве примера
-	const int test_matrix_lines = 4;//Изменяем размерность на MATRIX_SIZE
+	//const int test_matrix_lines = 4;//Изменяем размерность на MATRIX_SIZE
 
-	double **test_matrix = new double*[test_matrix_lines];
+	double **test_matrix = new double*[MATRIX_SIZE];
 
 	// цикл по строкам
-	for (i = 0; i < test_matrix_lines; ++i)
+	for (i = 0; i < MATRIX_SIZE; ++i)
 	{
 		// (test_matrix_lines + 1)- количество столбцов в тестовой матрице,
 		// последний столбец матрицы отведен под правые части уравнений, входящих в СЛАУ
-		test_matrix[i] = new double[test_matrix_lines + 1];
+		test_matrix[i] = new double[MATRIX_SIZE + 1];
 	}
 
 	// массив решений СЛАУ
-	double *result = new double[test_matrix_lines];
+	double *result = new double[MATRIX_SIZE];
 
 	// инициализация тестовой матрицы
 	/*test_matrix[0][0] = 2; test_matrix[0][1] = 5;  test_matrix[0][2] = 4;  test_matrix[0][3] = 1;  test_matrix[0][4] = 20;
@@ -104,16 +144,16 @@ int main()
 	test_matrix[3][0] = 3; test_matrix[3][1] = 8;  test_matrix[3][2] = 9;  test_matrix[3][3] = 2;  test_matrix[3][4] = 37;*/
 	InitMatrix(test_matrix);
 
-	SerialGaussMethod(test_matrix, test_matrix_lines, result);
-
-	for (i = 0; i < test_matrix_lines; ++i)
+	//SerialGaussMethod(test_matrix, MATRIX_SIZE, result);
+	ParallelGaussMethod(test_matrix, MATRIX_SIZE, result);
+	for (i = 0; i < MATRIX_SIZE; ++i)
 	{
 		delete[]test_matrix[i];
 	}
 
 	printf("Solution:\n");
 
-	for (i = 0; i < test_matrix_lines; ++i)
+	for (i = 0; i < MATRIX_SIZE; ++i)
 	{
 		printf("x(%d) = %lf\n", i, result[i]);
 	}
